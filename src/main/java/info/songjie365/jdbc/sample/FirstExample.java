75f019fdc36f3a9a279fce3c279657f423c7b406
songjie <songjiesdnu@163.com>
2015-07-17 22:10:42 +0800

    1、一个完成的简单例子； 2、ResultSet Navigation例子

---
+++ src/main/java/info/songjie365/jdbc/sample/FirstExample.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package info.songjie365.jdbc.sample;
+
+//第一步：引入相关的包
+import java.sql.*;
+
+/**
+ * @author songjie
+ *
+ */
+public class FirstExample {
+	//jdbc driver和database url
+	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String DB_RUL = "jdbc:mysql://localhost:3306/emp?useUnicode=true&characterEncoding=utf8";
+//	private static final String DB_RUL = "jdbc:mysql://localhost:3306/emp";
+	
+	//账号
+	private static final String USER = "root";
+	private static final String PASS = "123456";
+	
+	
+	public static void main(String[] args){
+		Connection conn = null;
+		Statement stmt = null;
+		try{
+			//第二步：注册jdbc driver
+			Class.forName(JDBC_DRIVER);
+			
+			//第三步：打开connection
+			conn = DriverManager.getConnection(DB_RUL, USER, PASS);
+			
+			//第四步：执行查询
+			stmt = conn.createStatement();
+			String sql = "select id,age,first,last from employees";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			//第五步：提取数据
+			while(rs.next()){
+				int id = rs.getInt("id");
+				int age = rs.getInt("age");
+				String first = rs.getString("first");
+				String last = rs.getString("last");
+				
+				System.out.println("id:" + id);
+				System.out.println("age:" + age);
+				System.out.println("first:" + first);
+				System.out.println("last:" + last);
+				System.out.println("***************");
+			}
+			
+			//第六步：清理执行环境，以释放资源
+			rs.close();
+			stmt.close();
+			conn.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			if(stmt != null){
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn != null){
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+}
---
+++ src/main/java/info/songjie365/jdbc/sample/ResultSetNavigation.java
@@ -0,0 +1,87 @@
+﻿/**
+ * 
+ */
+package info.songjie365.jdbc.sample;
+
+import java.sql.*;
+
+/**
+ * ResultSet Navigating
+ * @author songjie
+ *
+ */
+public class ResultSetNavigation {
+	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/emp?useUnicode=true&characterEncoding=utf8";
+	private static final String USER = "root";
+	private static final String PASS = "123456";
+	
+	public static void main(String[] args){
+		Connection conn = null;
+		Statement stmt = null;
+		try{
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			String sql = "select id, age, first, last from employees order by id desc";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			System.out.println("moving cursor to the last record");
+			rs.last();
+			int id = rs.getInt("id");
+			int age = rs.getInt("age");
+			String first = rs.getString("first");
+			String last = rs.getString("last");
+			printData(id, age, first, last);
+
+			System.out.println("moving cursor to the first record");
+			rs.first();
+			id = rs.getInt("id");
+			age = rs.getInt("age");
+			first = rs.getString("first");
+			last = rs.getString("last");
+			printData(id, age, first, last);
+
+			System.out.println("moving cursor to the nexr record");
+			rs.next();
+			id = rs.getInt("id");
+			age = rs.getInt("age");
+			first = rs.getString("first");
+			last = rs.getString("last");
+			printData(id, age, first, last);
+
+			rs.close();
+		}catch(Exception e){
+			
+		}finally{
+			if(stmt != null){
+				try{
+					stmt.close();
+				}catch(SQLException e){
+					e.printStackTrace();
+				}
+			}
+			if(conn != null){
+				try{
+					conn.close();
+				}catch(SQLException e){
+					e.printStackTrace();
+				}
+			}
+				
+		}
+		
+	}
+	/**
+	 *
+	 * 打印数据
+	 *
+	 */
+	private static void printData(int id, int age, String first, String last){
+			System.out.println("id:" + id);
+			System.out.println("age:" + age);
+			System.out.println("first:" + first);
+			System.out.println("last:" + last);
+	}
+		
+}
